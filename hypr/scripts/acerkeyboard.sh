02a004cda458418cc397870383ed1e3457ceddab
zeraaaaaaaaaa <152410176+zeraaaaaaaaaa@users.noreply.github.com>
2024-02-12 23:41:50 +0800

    Add files via upload

---
+++ hypr/scripts/acerkeyboard.sh
@@ -0,0 +1,4 @@
+$HOME/acer-keyboard/facer_rgb.py -m 0 -z 1 -b 100 -cR 30 -cG 30 -cB 46
+$HOME/acer-keyboard/facer_rgb.py -m 0 -z 2 -b 100 -cR 30 -cG 30 -cB 46
+$HOME/acer-keyboard/facer_rgb.py -m 0 -z 3 -b 100 -cR 30 -cG 30 -cB 46
+$HOME/acer-keyboard/facer_rgb.py -m 0 -z 4 -b 100 -cR 30 -cG 30 -cB 46
